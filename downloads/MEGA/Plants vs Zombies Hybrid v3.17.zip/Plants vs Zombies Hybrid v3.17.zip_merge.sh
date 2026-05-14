c3744a65d5be784b1fbac80d9761bea7991b654d
ramin00542 <39794336+ramin00542@users.noreply.github.com>
2026-05-14 16:21:46 +0330

    Delete downloads/MEGA directory

--- downloads/MEGA/Plants vs Zombies Hybrid v3.17.zip/Plants vs Zombies Hybrid v3.17.zip_merge.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# بازسازی فایل Plants vs Zombies Hybrid v3.17.zip از روی بخش‌ها
-cat "Plants vs Zombies Hybrid v3.17.zip.part"* > "Plants vs Zombies Hybrid v3.17.zip"
-echo "✅ فایل Plants vs Zombies Hybrid v3.17.zip بازسازی شد"
